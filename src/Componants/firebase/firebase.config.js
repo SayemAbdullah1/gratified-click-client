bc10a7950ad8810fc828aababac9a5187b4c34d2
SayemAbdullah1 <dxsayem0@gmail.com>
2022-11-10 21:32:35 +0600

    add env file

--- src/Componants/firebase/firebase.config.js
+++ src/Componants/firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyAqvYMsTh9rJfaDA9_iJf5EyXQfNACXo7E",
-    authDomain: "gratified-click.firebaseapp.com",
-    projectId: "gratified-click",
-    storageBucket: "gratified-click.appspot.com",
-    messagingSenderId: "175744665258",
-    appId: "1:175744665258:web:932e921f04216a65af32d3"
+    apiKey: process.env.REACT_APP_apiKey,
+    authDomain: process.env.REACT_APP_authDomain,
+    projectId: process.env.REACT_APP_projectId,
+    storageBucket: process.env.REACT_APP_storageBucket,
+    messagingSenderId: process.env.REACT_APP_messagingSenderId,
+    appId: process.env.REACT_APP_appId
 };
 
 // Initialize Firebase
